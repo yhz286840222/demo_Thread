2c35efbd39a19943feed69a13351a1db43ebc2fc
yanghz <286840222@qq.com>
2018-11-22 22:16:07 +0800

    disruptor 多生产者多消费者

--- less08_Thread/src/com/moudle/disruptorDemo/multi/Consumer.java
+++ less08_Thread/src/com/moudle/disruptorDemo/multi/Consumer.java
@@ -1,5 +1,24 @@
 package com.moudle.disruptorDemo.multi;
 
-public class Consumer {
+import java.util.concurrent.atomic.AtomicInteger;
 
+import com.lmax.disruptor.WorkHandler;
+
+public class Consumer implements WorkHandler<Order>{
+	private String consumerId;
+	private static AtomicInteger count=new AtomicInteger(0);
+	public Consumer(String consumerId){
+		this.consumerId=consumerId;
+	}
+
+	@Override
+	public void onEvent(Order order) throws Exception {
+		System.out.println("当前消费者："+this.consumerId+",消费消息："+order);
+		count.incrementAndGet();
+	}
+
+	public int getCount(){
+		return count.get();
+	}
+	
 }
